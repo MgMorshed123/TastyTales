4fa8791ee54c584756b2470b98f169c45836437b
mdarf <mdarfinji45@gmail.com>
2024-08-07 06:37:28 +0800

    creating navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
-  <>
-  <h1>hello</h1>
-  </>
-  )
+    <>
+      <div className="app">
+        <Navbar></Navbar>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
