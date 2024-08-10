f5324dde674f00a3ff4ef845e614cc299a58a290
mdarf <mdarfinji45@gmail.com>
2024-08-10 21:28:48 +0800

    adding headr components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your Favouwrite Foods</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+          provident.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Header from "../../components/Header/Header";
+import "./Home.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 };
 
 export default Home;
