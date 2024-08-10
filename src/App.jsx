636f3d2bf4443a262a3113ed99e8cf0d7be23964
mdarf <mdarfinji45@gmail.com>
2024-08-10 21:24:05 +0800

    adding initial route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       <div className="app">
         <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/cart" element={<Cart></Cart>} />
+          <Route path="/" element={<PlaceOrder></PlaceOrder>} />
+        </Routes>
       </div>
     </>
   );
