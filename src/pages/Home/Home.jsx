515e1994cd0cad97d87d31902c07d03518c5816e
mdarf <mdarfinji45@gmail.com>
2024-08-11 18:59:19 +0800

    styling food menu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import "./Home.css";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 const Home = () => {
   return (
     <div>
       <Header></Header>
+      <ExploreMenu></ExploreMenu>
     </div>
   );
 };
