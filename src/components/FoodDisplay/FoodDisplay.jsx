68c8b3e988167dc2200bea6340c77346fae53e28
mdarf <mdarfinji45@gmail.com>
2024-08-11 19:43:29 +0800

    showing food diplay

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./FoodDisplay.css";
+import { StoreContext } from "../../Context/StoreContext";
 
 const FoodDisplay = () => {
-  return <div>FoodDisplay</div>;
+  const { food_list } = useContext(StoreContext);
+
+  return (
+    <div className="food-display" id="food-display">
+      <he>Top Dishs Near Your</he>
+    </div>
+  );
 };
 
 export default FoodDisplay;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "../../components/Header/Header";
 import "./Home.css";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 const Home = () => {
   const [category, setCategory] = useState("All");
 
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
       <Header></Header>
       <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
+      <FoodDisplay category={category}></FoodDisplay>
     </div>
   );
 };
