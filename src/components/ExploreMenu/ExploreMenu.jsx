b81a2d387f31273781bd719e5ad3520928e2649d
mdarf <mdarfinji45@gmail.com>
2024-08-11 18:47:25 +0800

    adding explore menu

---
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ExploreMenu.css";
+
+const ExploreMenu = () => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore Our Menu</h1>
+      <p className="explore-menu-text">
+        Lorem, ipsum dolor sit amet consectetur adipisicing.
+      </p>
+      <div className="explore-menu-list"></div>
+    </div>
+  );
+};
+
+export default ExploreMenu;
