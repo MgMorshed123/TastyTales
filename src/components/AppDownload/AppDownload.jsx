cbf37e7e3ff4236b164580fdbb333b910c3fb062
mdarf <mdarfinji45@gmail.com>
2024-08-12 08:40:41 +0800

    adding app  download  css

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./AppDownload.css";
+import { assets } from "../../assets/food del assets/frontend_assets/assets";
 
 const AppDownload = () => {
-  return <div>AppDownload</div>;
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        For Better Experience Download <br /> Tomato App
+      </p>
+
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" srcset="" />
+        <img src={assets.app_store} alt="" srcset="" />
+      </div>
+    </div>
+  );
 };
 
 export default AppDownload;
